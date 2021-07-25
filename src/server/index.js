01ca7fa81bd4c04e37c2fe546ef1030c67a95127
Manuel Valles <manukempo@gmail.com>
2021-07-25 16:19:03 +0100

    Improve handling errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 const { Kafka } = require('kafkajs');
+const createApp = require('./app');
 const {
   kafka,
   app: { secret, mount, topic },
   server: { port },
 } = require('../config');
-const createApp = require('./app');
 
 const client = new Kafka(kafka);
 const producer = client.producer();
@@ -13,12 +13,38 @@ const main = async () => {
   await producer.connect();
   const app = createApp({ producer, secret, mount, topic });
 
-  app.listen(port, () => {
+  const server = app.listen(port, (error) => {
+    if (error) throw error;
+
     console.log(`Server listening on port ${port}`);
   });
+
+  const shutdown = async () => {
+    await server.close();
+    await producer.disconnect();
+  };
+
+  return shutdown;
 };
 
-main().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
+main()
+  .then(async (shutdown) => {
+    signalTraps.forEach((signal) => {
+      process.on(signal, async () => {
+        console.log(`Received ${signal} signal. Shutting down.`);
+        try {
+          await shutdown();
+          process.exit(0);
+        } catch (error) {
+          console.error('Error during shutdown', error);
+          process.exit(1);
+        }
+      });
+    });
+  })
+  .catch((error) => {
+    console.error('Error during startup', error);
+    process.exit(1);
+  });
